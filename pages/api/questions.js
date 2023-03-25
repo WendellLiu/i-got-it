dc938b8df0e3b30af77a4bf7970a148e8027a7ec
Wendell Liu <cuk.bas@gmail.com>
2023-03-25 23:18:33 +0800

    feat: integrate with openai

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -1,10 +1,36 @@
 import createHandler from "@/lib/handler/create-handler";
+import { Configuration, OpenAIApi } from "openai";
+
+const token = process.env.LOCAL_OPEN_AI_TOKEN;
+
+const configuration = new Configuration({
+  apiKey: token,
+});
+
+const generatePrompt = (
+  article,
+  n
+) => `Generate ${n} questions based on the following article
+${article}
+`;
+
+const openai = new OpenAIApi(configuration);
 
 const handler = createHandler({
-  post: (req, res) => {
+  post: async (req, res) => {
     const body = req.body;
+    const { article, nOfQuestions = 5 } = body;
 
-    res.status(200).json({ question: body });
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "user", content: generatePrompt(article, nOfQuestions) },
+      ],
+    });
+
+    const message = completion.data.choices[0].message;
+
+    res.status(200).json({ message });
   },
 });
 
